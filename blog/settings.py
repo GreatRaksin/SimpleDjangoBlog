a6114a9de655c15a3f6c8d91eb4329c9b7aa18a7
demidraksin <greatraksin@icloud.com>
2020-12-13 17:10:54 +0300

    Adding Post model

--- blog/settings.py
+++ blog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o6f+)yj-09y&kcox+h)@-dn26e-fc$_ef4mtx1c2(puma_cn9h'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mysite.apps.MysiteConfig',
-    'mysite',
 ]
 
 MIDDLEWARE = [
