be336dcf14da03b1d21e188832466941a2ef2bc5
demidraksin <greatraksin@icloud.com>
2020-12-20 18:30:26 +0300

    Adding new post form view

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -9,6 +10,12 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'mysite/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'mysite/post_detail.html', {'post': post})
+
+
+def new_post(request):
+    form = PostForm()
+    return render(request, 'mysite/post_edit.html', {'form': form})
